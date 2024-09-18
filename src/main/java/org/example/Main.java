a4fdf0c06a0c1c112665eeff9df522628683d641
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-09-18 16:50:05 -0300

    Estudando e comentando o código

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+//https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
+
 public class Main {
     public static void main(String[] args) throws IOException {
         //Define o modelo que será usado
@@ -28,22 +30,37 @@ public class Main {
         //Define o metodo de requisição como POST
         conexao.setRequestMethod("POST");
 
-        //Define o cabeçalho da requisição
-        //Content-Type é o nome do cabeçalho
-        //application/
+        //Define o cabeçalho da requisição (header)
+        //Content-Type é o nome do cabeçalho que define o tipo da requisição
+        //application/json; define o tipo do do arquivo que será enviado oara o servidor
+        //utf-8 define o charset que esta sendo utilizado para codificar conteúdo da requisição
         conexao.setRequestProperty("Content-Type", "application/json; utf-8");
 
+        //Accept é o nome do cabeçalho que define o tipo do retorno da requição
+        //application/json define que será retornado um arquivo JSON
         conexao.setRequestProperty("Accept", "application/json");
-        conexao.setDoOutput(true); //
 
+        //setDoOutput(true) stá dizendo que pretende enviar dados na requisição HTTP
+        // ou seja, que o corpo (body) da requisição terá conteúdo
+        //É necessário quando você deseja fazer requisições que envolvem o envio de dados (POST)
+        conexao.setDoOutput(true);
 
-        String jsonInputString = String.format(
+        //Criando o corpo (body) da requisição
+        //Usando o String.format() para formatar a string, substintuindo os %s pelas variáveis
+        String body = String.format(
                 "{\"model\": \"%s\", \"prompt\":\"%s\", \"images\": \"%s\", \"stream\": false}", nomeModelo, promp, imagem
         );
 
-
+        //O OutputStream permite enviar dados no corpo da requisição para o servidor
         try(OutputStream os = conexao.getOutputStream()){
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            //O metodo OutputStream trabalha diretamente com bytes
+            //getBytes converte o body para um array de bytes
+            //StandardCharsets.UTF-8 faz os caracteres serem corretamente convertidos para bytes no formato UTF-8
+            byte[] input = body.getBytes(StandardCharsets.UTF_8);
+
+            //os.write() é usado para escrever os bytes no OutputStream
+            //oO seja, para enviar o conteúdo da variável body no corpo da requisição HTTP
+            //É passado o input (body), ponto inicial do array (0) e o ponto final(input.length)
             os.write(input, 0, input.length);
         }
 
