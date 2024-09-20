3ae7483bf82168974bbd497ec269bd04eb98bae8
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-09-20 17:05:18 -0300

    testes com modelo minicpm-v

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import org.json.JSONObject;
-
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 //https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
@@ -16,10 +11,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         //Define o modelo que será usado
-        String nomeModelo = "llava";
+        //String nomeModelo = "llava";
+        String nomeModelo = "minicpm-v";
+
 
         String img = ImagesToBase64.encodeImageToBase64(new File("C:\\Users\\eduar\\OneDrive\\Área de Trabalho\\api\\documentos\\rg verso.jpeg"));
         //Define o prompt que será enviado para o modelo
+
 
         //String promp = lerPromp("prompt: ");
 
@@ -31,9 +29,12 @@ public class Main {
         String prompt2 = "Extract the set of 11 numbers that are present after the word CPF";
         String prompt3 = "Extract the set of 11 numbers that are present in the upper left corner, these numbers are preceded by the word CPF";
         String prompt4 = "Extract the 11 CPF numbers in the upper left corner of the image";
+        String prompt6 = "Extract the set of 9 numbers that are present after the word 'Registro Geral'";
+        String prompt5 = "Extract all text from image"; //Não é um bom prompt
+        String prompt7 = "extract the sets of numbers present after the words CPF and RG";
 
-        System.out.print("Carregando");
-        String responseText = Ollama.GetResponse(nomeModelo, prompt4, img);
+        System.out.print("Carregando...\n");
+        String responseText = Ollama.GetResponse(nomeModelo, prompt7, img);
 
         GerarTXT.gerarTexto(responseText);
     }
